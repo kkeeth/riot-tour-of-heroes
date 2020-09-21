fb799b8ca8b54b428a13453123c1d5e219e6a495
kkeeth <zensin0082@gmail.com>
2020-09-21 14:57:27 +0900

    refactor: divided heroes component into list and detail components

--- src/register-global-components.js
+++ src/register-global-components.js
@@ -1,18 +1,22 @@
-import { register } from 'riot'
-import { basename } from 'path'
+import { register } from "riot";
+import { basename } from "path";
 
-const globalComponentsContext = require.context('./components/global/', true, /[a-zA-Z0-9-]+\.riot/)
+const globalComponentsContext = require.context(
+  "./components/global/",
+  true,
+  /[a-zA-Z0-9-]+\.riot/
+);
 
 export default () => {
-  globalComponentsContext.keys().map(path => {
-    const name = basename(path, '.riot')
-    const component = globalComponentsContext(path)
+  globalComponentsContext.keys().map((path) => {
+    const name = basename(path, ".riot");
+    const component = globalComponentsContext(path);
 
-    register(name, component.default)
+    register(name, component.default);
 
     return {
       name,
-      component
-    }
-  })
-}
+      component,
+    };
+  });
+};
